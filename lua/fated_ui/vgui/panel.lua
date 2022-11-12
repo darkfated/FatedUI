59bb4d9f666bf836c59fe32d2dfaf40c296dad48
Dark Fated <darkfated@icloud.com>
2022-11-12 04:11:42 +0300

    PANEL: radius setting

--- lua/fated_ui/vgui/panel.lua
+++ lua/fated_ui/vgui/panel.lua
@@ -2,14 +2,19 @@ local ELEMENT = {}
 
 function ELEMENT:Init()
 	self.color_id = 1
+	self.rad = 6
 end
 
 function ELEMENT:Paint( w, h )
-	draw.RoundedBox( 6, 0, 0, w, h, FatedUI.col.panel_table[ GetConVar( 'fated_ui_color_background' ):GetString() ][ self.color_id ] )
+	draw.RoundedBox( self.rad, 0, 0, w, h, FatedUI.col.panel_table[ GetConVar( 'fated_ui_color_background' ):GetString() ][ self.color_id ] )
 end
 
 function ELEMENT:Color( id )
 	self.color_id = id
+end
+
+function ELEMENT:Radius( rad )
+	self.rad = rad
 end
 
 vgui.Register( 'fu-panel', ELEMENT, 'DPanel' )
